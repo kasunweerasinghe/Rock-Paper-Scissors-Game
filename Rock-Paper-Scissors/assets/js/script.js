1538f38335f6c2e4949e63500bf76810184e6771
Kasun-Weerasinghe <kasunrachintha2018@gmail.com>
2022-10-18 17:05:12 +0530

    load rock paper and scissors images to window using js

---
+++ Rock-Paper-Scissors/assets/js/script.js
@@ -0,0 +1,18 @@
+var you;
+var yourScore = 0;
+var opponent;
+var opponentScore = 0;
+
+var choices = ["rock", "paper", "scissors"];
+
+window.onload = function (){
+    for(let i=0; i<3; i++){
+
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        document.getElementById("choices").append(choice);
+
+    }
+}
+
