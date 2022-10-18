fa19144143d3642057fc377beff6dca13174c330
Kasun-Weerasinghe <kasunrachintha2018@gmail.com>
2022-10-18 17:28:47 +0530

    set score to h1 tags

--- Rock-Paper-Scissors/assets/js/script.js
+++ Rock-Paper-Scissors/assets/js/script.js
@@ -17,11 +17,41 @@ window.onload = function () {
 }
 
 //added images when we press
-function selectChoice(){
+function selectChoice() {
     you = this.id;
     document.getElementById('your-choice').src = you + '.png';
 
     //random for opponent
     opponent = choices[Math.floor(Math.random() * 3)];
+    document.getElementById('opponent-choice').src = opponent + '.png';
 
+    //check winner
+    if (you == opponent) {
+        yourScore += 1;
+        opponentScore += 1;
+    } else {
+        if (you == "rock") {
+            if (opponent == "scissors") {
+                yourScore += 1;
+            } else if (opponent == "paper") {
+                opponentScore += 1;
+            }
+        } else if (you == "scissors") {
+            if (opponent == "paper") {
+                yourScore += 1;
+            } else if (opponent == "rock") {
+                opponentScore += 1;
+            }
+        } else if (you == "paper") {
+            if (opponent == "rock") {
+                yourScore += 1;
+            } else if (opponent == "scissors") {
+                opponentScore += 1;
+            }
+        }
+    }
+
+    //set score to h1's
+    document.getElementById('your-score').innerText = yourScore;
+    document.getElementById('opponent-score').innerText = opponentScore;
 }
