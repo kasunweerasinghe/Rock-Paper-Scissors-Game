c2942cd5d7f6b10512888a9bc8791d213f4acccc
Kasun-Weerasinghe <kasunrachintha2018@gmail.com>
2022-10-18 17:09:58 +0530

    added images when we press

--- Rock-Paper-Scissors/assets/js/script.js
+++ Rock-Paper-Scissors/assets/js/script.js
@@ -5,14 +5,19 @@ var opponentScore = 0;
 
 var choices = ["rock", "paper", "scissors"];
 
-window.onload = function (){
-    for(let i=0; i<3; i++){
+window.onload = function () {
+    for (let i = 0; i < 3; i++) {
 
         let choice = document.createElement("img");
         choice.id = choices[i];
         choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
-
     }
 }
 
+//added images when we press
+function selectChoice(){
+    you = this.id;
+    document.getElementById('your-choice').src = you + '.png';
+}
